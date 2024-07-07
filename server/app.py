accaf05c2012761229c43da72372f20757a0741a
Favor Vincent <vfavorm77@gmail.com>
2024-07-07 15:52:13 +0300

    passwords lab complete

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, make_response
 from flask_restful import Resource
 
 from config import app, db, api
@@ -28,16 +28,39 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
+    def get(self):
+        if 'user_id' in session:
+            user = db.session.get(User, session['user_id'])
+            return user.to_dict(), 200
+        else:
+            return {}, 204
+
     pass
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return make_response(user.to_dict() ,200)
+        return {'error': 'Invalid username or password'}, 401
 
 class Logout(Resource):
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
     pass
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
